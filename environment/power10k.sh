a7f04f5e62fd98b32c5684355823d9a771d717df
Htet-Phyo-Linn <htetphyolin642@gmail.com>
2023-07-18 15:42:59 +0630

    update redhatnote

--- environment/power10k.sh
+++ environment/power10k.sh
@@ -1,14 +1,16 @@
+#!/usr/bin/bash
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-#change config
-vim .zshrc
-powerlevel10k/powerlevel10k
+#config
+apt install fonts-powerline
+p10k configure
 
 #fonts for terminal
+mkdir ~/.local/share/fonts
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 mv *.ttf ~/.local/share/fonts/
 
-#config
-sudo apt install fonts-powerline
-p10k configure
-
+#change config
+vim .zshrc
+#powerlevel10k/powerlevel10k
