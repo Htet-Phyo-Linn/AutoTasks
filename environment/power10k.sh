1bdf5a99ac7f209ab76f8bf9f93071cff1b73f06
Htet Phyo Linn <109867841+Htet-Phyo-Linn@users.noreply.github.com>
2023-07-25 11:09:37 +0630

    Update power10k.sh

--- environment/power10k.sh
+++ environment/power10k.sh
@@ -2,15 +2,23 @@
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+
 #config
 apt install fonts-powerline
-p10k configure
+
 
 #fonts for terminal
+rm -rf ~/.local/share/fonts
+
 mkdir ~/.local/share/fonts
+
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+
 mv *.ttf ~/.local/share/fonts/
+
 
 #change config
 vim .zshrc
 #powerlevel10k/powerlevel10k
+
+p10k configure
